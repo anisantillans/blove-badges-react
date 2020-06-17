eab70e0da6a5a9ce6dd69268d56a137f0d15ee7c
Ana Paula Santillan <pau.santillan17@gmail.com>
2020-06-17 02:00:20 -0500

    fix index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"; //analogous of createElement
 import ReactDom from "react-dom"; //analogous of appendChild
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
-import BadgeNew from "./pages/BadgeNew";
 import Badges from "./pages/Badges";
 
 const container = document.getElementById("app");
